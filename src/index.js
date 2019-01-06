cd2674042bf39d565f9c09079bcd6a79dd079f49
Martin <mr.mmaurer+work@gmail.com>
2019-01-06 21:02:08 +0100

    Added global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { createGlobalStyle } from 'styled-components';
+
 import Game from './Game/Game';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+// import './index.css';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    background-color: #000;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyles />
+        <Game />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
