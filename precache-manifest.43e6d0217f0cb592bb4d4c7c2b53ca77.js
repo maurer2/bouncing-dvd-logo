d3b9d46fa59ef9aaf03ec85f431b5faecbdb298c
Martin <mr.mmaurer+work@gmail.com>
2019-09-14 00:54:35 +0100

    Updates

---
+++ precache-manifest.43e6d0217f0cb592bb4d4c7c2b53ca77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f2cd603d55993507f71800de36ecb1",
+    "url": "/bouncing-dvd-logo/index.html"
+  },
+  {
+    "revision": "b7c99a41392120b36b54",
+    "url": "/bouncing-dvd-logo/static/js/2.969a69d3.chunk.js"
+  },
+  {
+    "revision": "04a735810e5fcf932886",
+    "url": "/bouncing-dvd-logo/static/js/main.c905979d.chunk.js"
+  },
+  {
+    "revision": "eadf6a80db433b9640e2",
+    "url": "/bouncing-dvd-logo/static/js/runtime~main.e2eced9a.js"
+  },
+  {
+    "revision": "935cf52479c4d937b8f0f93905b5ecda",
+    "url": "/bouncing-dvd-logo/static/media/cat.935cf524.svg"
+  },
+  {
+    "revision": "61bf12c2ae48206c2180eb67f8f7b26c",
+    "url": "/bouncing-dvd-logo/static/media/soundFile.61bf12c2.wav"
+  }
+]);
