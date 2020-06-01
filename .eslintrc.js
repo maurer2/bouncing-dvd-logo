9bdcd6d87c6f0463c377a349e54a735a603d4a75
Martin <mr.mmaurer+work@gmail.com>
2020-06-01 23:16:42 +0200

    Renamed files to ts and tsx and initiated ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,16 @@ module.exports = {
         ],
       },
     ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
   parserOptions: {
     ecmaFeatures: {
