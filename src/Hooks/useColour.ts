44d71e96bdbadb652f8baf611fc2527b9affff34
Martin <mr.mmaurer+work@gmail.com>
2020-06-09 21:54:22 +0100

    Changed name of setter

--- src/Hooks/useColour.ts
+++ src/Hooks/useColour.ts
@@ -1,10 +1,10 @@
 import { useState, useCallback } from 'react';
 import { random } from 'lodash';
 
-export default function useColour(colours): Readonly<[any, () => void]> {
+export default function useColour(colours: string[]): Readonly<[string, () => void]> {
   const [colour, setColour] = useState(colours[0]);
 
-  const setNewColour = useCallback(() => {
+  const changeColour = useCallback(() => {
     const newColours = colours.filter((colourEntry) => colourEntry !== colour);
     const randomColourIndex = random(newColours.length - 1);
     const newColour = newColours[randomColourIndex];
@@ -12,5 +12,5 @@ export default function useColour(colours): Readonly<[any, () => void]> {
     setColour(newColour);
   }, [colour, colours]);
 
-  return [colour, setNewColour] as const;
+  return [colour, changeColour] as const;
 }
