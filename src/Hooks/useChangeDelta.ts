97bc03049e91bbbfec1f616041c6d1aed9f63f7f
Martin <mr.mmaurer+work@gmail.com>
2020-06-19 21:39:07 +0100

    Modified useChangeDelta to only deal with a single axis

--- src/Hooks/useChangeDelta.ts
+++ src/Hooks/useChangeDelta.ts
@@ -4,13 +4,11 @@ import { random } from 'lodash';
 // Readonly<[ReturnType<typeof useRef>
 
 export default function useChangeDelta(
-  hasCollidedX: boolean,
-  hasCollidedY: boolean,
-): Readonly<[MutableRefObject<number>, MutableRefObject<number>]> {
-  const hasCollidedXPrev = useRef(false);
-  const hasCollidedYPrev = useRef(false);
-  const changeDeltaX = useRef(2);
-  const changeDeltaY = useRef(2);
+  startValue: number,
+  hasCollided: boolean,
+): Readonly<[MutableRefObject<number>]> {
+  const hasCollidedPrev = useRef(false);
+  const changeDelta = useRef(startValue);
 
   const maxRandomness = 10; // max value of deviation from correct reflection on collision
   const upperRandomBound = 1.0 + ((maxRandomness / 2) / 100);
@@ -18,35 +16,20 @@ export default function useChangeDelta(
 
   // init
   useEffect(() => {
-    changeDeltaX.current = (random(1) === 0) ? changeDeltaX.current * -1 : changeDeltaX.current * +1;
-    changeDeltaY.current = random(1) === 0 ? changeDeltaY.current * -1 : changeDeltaY.current * +1;
+    changeDelta.current = (random(1) === 0) ? changeDelta.current * -1 : changeDelta.current * +1;
   }, []);
 
   useEffect(() => {
-    if (hasCollidedX && !(hasCollidedXPrev.current)) {
-      let newChangeDelta = changeDeltaX.current * random(lowerRandomBound, upperRandomBound, true);
+    if (hasCollided && !(hasCollidedPrev.current)) {
+      const newChangeDelta = changeDelta.current * random(lowerRandomBound, upperRandomBound, true);
 
-      newChangeDelta = (Math.sign(newChangeDelta) === 1) ? Math.abs(newChangeDelta) * -1 : Math.abs(newChangeDelta);
-
-      changeDeltaX.current = newChangeDelta;
+      changeDelta.current = newChangeDelta * -1;
     }
-  }, [hasCollidedX, upperRandomBound, lowerRandomBound]);
+  }, [hasCollided, upperRandomBound, lowerRandomBound]);
 
   useEffect(() => {
-    if (hasCollidedY && !(hasCollidedYPrev.current)) {
-      let newChangeDelta = changeDeltaY.current * random(lowerRandomBound, upperRandomBound, true);
-
-      newChangeDelta = (Math.sign(newChangeDelta) === 1) ? Math.abs(newChangeDelta) * -1 : Math.abs(newChangeDelta);
-
-      changeDeltaY.current = newChangeDelta;
-    }
-  }, [hasCollidedY, upperRandomBound, lowerRandomBound]);
-
-  // keep track of previous collision state
-  useEffect(() => {
-    hasCollidedXPrev.current = hasCollidedX;
-    hasCollidedYPrev.current = hasCollidedY;
+    hasCollidedPrev.current = hasCollided;
   });
 
-  return [changeDeltaX, changeDeltaY] as const;
+  return [changeDelta] as const;
 }
