dbede2ce8a909c0560c7ceff5eb09fd90253b4ee
Martin <mr.mmaurer+work@gmail.com>
2022-11-14 23:15:14 +0100

    Removed unneeded extensions and added bundle analyzer equivalent for vite/rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import svgrPlugin from 'vite-plugin-svgr'; // needed top import SVG as React components, e.g. import ReactComponent as X
+import { visualizer } from 'rollup-plugin-visualizer';
+
+import type { PluginOption } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: false,
   },
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    visualizer({
+      template: 'treemap',
+      filename: 'statistics/statistics.html',
+      gzipSize: true,
+      brotliSize: true,
+      open: true,
+    }) as PluginOption
+  ],
 });
