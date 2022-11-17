4e5e686eef7619cbbd1d89aa55c102c5674d9c5d
Martin <mr.mmaurer+work@gmail.com>
2022-11-17 20:42:29 +0100

    Added connector for redux devtools and added redux eslint rules

--- src/Store2/index.ts
+++ src/Store2/index.ts
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import type { Store as StoreRedux } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import type { Store, Action } from './types';
 import reducers from './reducers';
@@ -8,6 +9,11 @@ import reducers from './reducers';
 // https://www.freecodecamp.org/news/how-to-use-redux-in-your-react-typescript-app/
 const store: StoreRedux<Store, Action> & {
   dispatch: (action: Action) => Action;
-} = createStore(reducers, applyMiddleware(logger));
+} = createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(logger),
+  )
+);
 
 export default store;
