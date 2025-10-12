b258429734408425623cb23a3360ec05579a4c92
Martin <mr.mmaurer@gmail.com>
2025-10-12 21:42:46 +0200

    Fixed snapshot serializer end reenabeld snapshots

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -1,5 +1,11 @@
 import '@testing-library/jest-dom/vitest';
 import * as matchers from 'jest-extended';
+import { expect } from 'vitest';
+// https://brightinventions.pl/blog/snapshot-testing-styled-components-with-vitest/
+// @ts-ignore
+import { styleSheetSerializer } from 'jest-styled-components/serializer';
+
+expect.addSnapshotSerializer(styleSheetSerializer as any);
 
 vi.mock('zustand'); // to make it work like Jest (auto-mocking)
 expect.extend(matchers);
